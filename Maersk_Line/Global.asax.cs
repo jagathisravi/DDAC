92bf5c787f5e33720fb4c49df06c3ce7b0b55852
jagathis ravichandrab <jagathis ravichandrab>
2017-07-11 04:53:30 +0800

    Add project files.

---
+++ Maersk_Line/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace Maersk_Line
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        private static ConnectionMultiplexer redisCache;
+
+        public static ConnectionMultiplexer RedisCache { get { if (redisCache == null || !redisCache.IsConnected)
+                {
+                    redisCache = ConnectionMultiplexer.Connect(ConfigurationManager.ConnectionStrings["RedisConnection"].ConnectionString);
+                }
+                return redisCache;
+            }
+        }
+       
+    }
+}
